dae80ad9188d77b8c4d24894faf88c2f946d2132
Pierre Thonon <pierre.thonon@gmail.com>
2025-02-19 09:25:57 +0100

    Ajustement hauteur du logo

--- Simple_TTS_GUI.py
+++ Simple_TTS_GUI.py
@@ -266,6 +266,7 @@ class MainWindow(QMainWindow):
                 background-color: transparent;
                 padding: 5px;
                 margin-right: 10px;
+                margin-top: -40px;
             }
         """)
         header_layout.addWidget(logo_label)
